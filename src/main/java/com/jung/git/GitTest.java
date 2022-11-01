9a8d762fed1b8c33de36a42e486065dfc8dc8054
Jung <Jung@Kim.com>
2022-11-01 10:09:51 +0800

    second commit

--- src/main/java/com/jung/git/GitTest.java
+++ src/main/java/com/jung/git/GitTest.java
@@ -3,6 +3,7 @@ package com.jung.git;
 public class GitTest {
     public static void main(String[] args) {
        System.out.println( "hello Kimyoujung！");
+        System.out.println( "hello jinyuzhen！");
 
     }
 }
