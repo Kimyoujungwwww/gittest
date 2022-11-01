ef42440045b602083f5bf4e228352806e7b6f8f9
Jung <Jung@Kim.com>
2022-11-01 10:11:36 +0800

    third commit

--- src/main/java/com/jung/git/GitTest.java
+++ src/main/java/com/jung/git/GitTest.java
@@ -3,7 +3,8 @@ package com.jung.git;
 public class GitTest {
     public static void main(String[] args) {
        System.out.println( "hello Kimyoujung！");
-        System.out.println( "hello jinyuzhen！");
+       System.out.println( "hello jinyuzhen！");
+       System.out.println( "hello world！");
 
     }
 }
